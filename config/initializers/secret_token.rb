0b7a1d8e8211e627e14ae4c50b62b335b8e6c102
Owen Moony <owenm@boxdice.com.au>
2011-08-28 08:26:12 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Btrain::Application.config.secret_token = '990b8a0b3fb2e518867103db1516ac10a4b47e814a67a749a9ecf4919acb2cc8c7e95dc49311d5e4e4889c544a2d9b33520427d686b6be9d9979a501fb581f9e'
